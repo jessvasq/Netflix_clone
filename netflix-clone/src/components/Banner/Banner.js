94ce26666eaede059b7e38e681f5813ca8bc2a28
User Name <jessnsq.codes@gmail.com>
2024-02-27 12:31:06 -0500

    api, fetch data, banner component

--- netflix-clone/src/components/Banner/Banner.js
+++ netflix-clone/src/components/Banner/Banner.js
@@ -1,27 +1,47 @@
-import React from 'react'; 
+import React, { useEffect, useState } from 'react'; 
 import './Banner.css';
-
+import axios from '../../axios';
+import requests from '../../requests';
 
 function Banner() {
+
+    const [movie, setMovie] = useState([]);
+    
+    //fetch a random movie from the netflix originals list by using a async function
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(requests.fetchNetflixOriginals);
+            setMovie(
+                request.data.results[
+                    //get a random movie from the list
+                    Math.floor(Math.random() * request.data.results.length - 1)
+                ]
+            );
+            return request;
+        }
+        //call the function to fetch the data
+        fetchData();
+    }, []);
+
     function truncate(string, n) {
         return string?.length > n ? string.substr(0, n-1) + '...' : string;
     }
         
     return (
         <header className='banner' style={{
-            backgroundImage: `url('https://www.xfire.com/wp-content/uploads/2022/09/netflix-cover-2-800x450.png')`, 
+            backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`, 
             backgroundSize: 'cover', 
             backgroundPosition: 'center center'
         }}>
 
             <div className='banner-content'>
-                <h1 className='banner-title'>Movie Name</h1>
+                <h1 className='banner-title'>{ movie?.title || movie?.name || movie?.original_name } </h1>
                 <div className='banner-buttons'>
                     <button className='banner-bttn'>Play</button>
                     <button className='banner-bttn'>My List</button>
                 </div>
                 <h1 className='banner-desc'> 
-                {truncate(`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`, 180)}
+                {truncate(movie?.overview, 180)}
                 </h1>
             </div>
 
