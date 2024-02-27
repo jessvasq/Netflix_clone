9f6a3e559f004a2ff4852ae29aa32e5f8cc7b47e
User Name <jessnsq.codes@gmail.com>
2024-02-26 19:54:23 -0500

    setup

---
+++ netflix-clone/src/components/Home/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'; 
+import './Home.css'; 
+import Nav from '../NavBar/Nav';
+
+function Home() {
+  return (
+    <div className='homeScreen'>
+      {/* Nav */}
+      <Nav />
+
+      {/* Banner */}
+
+      {/* Lists */}
+
+
+    </div>
+  )
+}
+
+export default Home
---
+++ netflix-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDLSibYOnCxWH5uUkpj1BFkAdwXGyS5AQ8",
+    authDomain: "netflix-clone-68046.firebaseapp.com",
+    projectId: "netflix-clone-68046",
+    storageBucket: "netflix-clone-68046.appspot.com",
+    messagingSenderId: "369346335414",
+    appId: "1:369346335414:web:1564852ff4a41a94714c17"
+  };
